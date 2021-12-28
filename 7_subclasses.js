19877a36e51b16e490b1d663393ed933c7460c98
Tiago Gomes <tidgomes@gmail.com>
2021-12-28 16:42:07 -0300

    subclasses

---
+++ 7_subclasses.js
@@ -0,0 +1,22 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}.`;
+  }
+}
+
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+
+const mag1 = new Magazine("turma dos manos", "mano de souza", "1995", "jun");
+
+console.log(mag1);
